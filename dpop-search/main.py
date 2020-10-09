604d61b979449cc5c8a473dd96c794f3712f0c2b
ryan-vb <ryanvanbaardwijk@gamil.com>
2020-10-09 22:08:31 +0100

    Update main.py

--- dpop-search/main.py
+++ dpop-search/main.py
@@ -1,30 +1,15 @@
 from bs4 import BeautifulSoup
-# import rpa as r
+from selenium import webdriver
 
-from statistics import mean
-
-
-
-
-def get_price_range(soup: BeautifulSoup):
-    prices_elements = soup.find_all(attrs={"aria-label": "Price"})
-
-    price_float = list(map(lambda x: float(x.text[1:]), prices_elements))
-
-    price_min = min(price_float)
-    price_max = max(price_float)
-    price_mean = mean(price_float)
-
-    return {'max': price_max, 'min': price_min, 'mean': price_mean}
+from . import scrape
+from . import parse
 
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
-    page_html = dpop_search(driver, "main squeeze eyeshadow palette")
-    driver.close()
-
+    page_html = scrape.dpop_search(driver, "main squeeze eyeshadow palette")
     page_soup = BeautifulSoup(page_html)
-    price_range = get_price_range(page_soup)
+    price_range = parse.get_price_range(page_soup)
 
     print("min: " + price_range["min"])
     print("mean: " + price_range["mean"])
